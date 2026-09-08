61e9625faf08d09cad4d0c4e645aad308bf8ced3
BicicletariaMayBike <douglasomendes2510@gmail.com>
2026-09-08 13:58:04 -0300

    V18.0.1 atualiza cache com correção da bicicleta na venda

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'maybike-v18-pdv-cache-v1';
+const CACHE_NAME = 'maybike-v18-pdv-cache-v2';
 const APP_SHELL = [
   './','./index.html','./style.css',
-  './app.js?v=1800','./pwa.js?v=1800','./hotfix-v155.js?v=1800','./hotfix-modulos-v155.js?v=1800',
-  './v156-estavel.js?v=1800','./v156-movimentacoes-caixa.js?v=1800','./v160-venda.js?v=1800','./v161-recibo.js?v=1800',
-  './v161-os-separadas.js?v=1800','./v170-gestao.js?v=1800','./v170-fix-mao-duplicada.js?v=1800','./v1702-os-itens-separados.js?v=1800',
-  './v1703-impressao-profissional.js?v=1800','./v1704-faturamento-os.js?v=1800','./v180-creditos-recibos.js?v=1800','./v180-recibos.js?v=1800','./v180-crm.js?v=1800',
+  './app.js?v=1801','./pwa.js?v=1801','./hotfix-v155.js?v=1801','./hotfix-modulos-v155.js?v=1801',
+  './v156-estavel.js?v=1801','./v156-movimentacoes-caixa.js?v=1801','./v160-venda.js?v=1801','./v161-recibo.js?v=1801',
+  './v161-os-separadas.js?v=1801','./v170-gestao.js?v=1801','./v170-fix-mao-duplicada.js?v=1801','./v1702-os-itens-separados.js?v=1801',
+  './v1703-impressao-profissional.js?v=1801','./v1704-faturamento-os.js?v=1801','./v180-creditos-recibos.js?v=1801','./v180-recibos.js?v=1801','./v180-crm.js?v=1801','./v1801-bike-venda.js?v=1801',
   './manifest.json','./icons/icon-192.png','./icons/icon-512.png','./icons/maybike-logo.png','./icons/maybike-logo-horizontal.png','./icons/maybike-logo-print.png'
 ];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)));self.skipWaiting();});
